c51e056cd77e1bce139802efa1d31c2de62f7307
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2021-12-23 13:15:21 +0600

    Image optimization and icon bug fix

--- src/presentation/common/block/Promotion.tsx
+++ src/presentation/common/block/Promotion.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { HOME } from 'constant/routes';
 
 const Promotion = () => (
-    <div className="ps-home-promotions">
-        <Link href={HOME}>
-            <img src="/static/img/promotions/home-5/simple.jpg" alt="martfury" />
-        </Link>
+    <div className="ps-search-trending">
+        <div className="container">
+            <Link href={HOME}>
+                <Image width={1200} height={245} src="/static/img/promotions/home-5/simple.jpg" alt="martfury" />
+            </Link>
+        </div>
     </div>
 );
 
